ddaa24adf0d4b2b8442f93a0e86589417cdc2f80
hdfmacedo <hdfmacedo+git@gmail.com>
2021-08-04 17:12:10 -0300

    revisando for-in e for-of

--- importante/for_in_objects.js
+++ importante/for_in_objects.js
@@ -6,3 +6,28 @@ const pessoa = {
 for(let chave in pessoa) {
     console.log(chave, pessoa[chave]);
 }
+
+// interação sobre arrays...
+const cores = ['Vermelho', 'Azul', 'Amarelo'];
+
+for(let indice in cores) {
+    console.log(indice, cores[indice]);
+}
+
+//for-of
+for (const cor of cores) {
+    console.log(cor);
+}
+
+// not defined aqui embaixo, tanto indice quanto cor
+//for_in_objects.js:23 Uncaught ReferenceError: cor is not defined
+//at for_in_objects.js:23
+try {
+    console.log(indice);
+} catch(Er) {
+    console.error(Er);
+}try {
+    console.log(cor);
+} catch(Er) {
+    console.error(Er);
+}
