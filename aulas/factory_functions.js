e8c3fd5b97f15857191e1cc24a08116d480e1c03
hdfmacedo <hdfmacedo+git@gmail.com>
2021-08-05 11:39:15 -0300

    factory_functions

---
+++ aulas/factory_functions.js
@@ -0,0 +1,13 @@
+const celular1 = criarCelular("ASUS 2", {vertical: 155, horizontal: 75});
+console.log(celular1);
+celular1.ligar();
+
+function criarCelular(marca, tamanhoTela) {
+    return {
+        marca
+        , tamanhoTela
+        , ligar() {
+            console.log("Fazendo ligação ["+(this.marca)+"]");
+        }
+    }
+}
