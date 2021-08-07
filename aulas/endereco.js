c82d8974c9e16cb504d1af0c721a01543785d95c
hdfmacedo <hdfmacedo+git@gmail.com>
2021-08-06 22:18:47 -0300

    exercicio objeto for-in

---
+++ aulas/endereco.js
@@ -0,0 +1,18 @@
+class Endereco {
+    constructor(rua, cidade, cep) {
+        this.rua = rua;
+        this.cidade = cidade;
+        this.cep = cep;
+    }
+
+
+}
+
+function exibirEndereco(endereco) {
+    for(let data in endereco) {
+        console.log(data, endereco[data]);
+    }
+}
+
+let end = new Endereco("Abram Goldszstein", "POA", "91500-500");
+exibirEndereco(end);
